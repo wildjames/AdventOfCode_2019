df782ce43a5b4dcc4d3009c107d34218185c2b19
wildjames <jwild2@sheffield.ac.uk>
2019-12-09 22:59:38 +0000

    Day 8 python solution

---
+++ day8.py
@@ -0,0 +1,55 @@
+import numpy as np
+
+def decode_image(img, x_size, y_size):
+    'Take img as a string'
+    layers = []
+
+    img = np.array([int(n) for n in img])
+
+    print("img is {} characters.".format(img.shape))
+    n_layers = len(img) // (x_size*y_size)
+    print("There are {} layers to the image".format(n_layers))
+
+
+    layers = img.reshape((n_layers, y_size, x_size))
+
+    return layers
+
+
+# Part one
+with open('day8.txt', 'r') as image_file:
+    img = image_file.read().strip()
+
+image = decode_image(img, 25, 6)
+
+N_zeros = 25*6
+target_layer = np.array([])
+for layer in image:
+    if np.sum(layer==0) < N_zeros:
+        N_zeros = np.sum(layer==0)
+        target_layer = layer
+
+print("Part 1 answer: {}".format(
+        np.sum(target_layer==1)*np.sum(target_layer==2)
+    )
+)
+
+# Part 2
+# 0: Black
+# 1: White
+# 2: Transparent
+# Image is rendered by stacking directly on top of each other.
+
+import matplotlib.pyplot as plt
+def render_image(image):
+    # Image starts as first layer
+    render = image[-1]
+    for i, layer in enumerate(image[-1::-1]):
+        locs = np.where(layer != 2)
+        render[locs] = layer[locs]
+
+    return render
+
+render = render_image(image)
+plt.imshow(render)
+plt.show()
